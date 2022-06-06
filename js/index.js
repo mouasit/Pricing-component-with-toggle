ff301f962cba75df515dcead7c11e65bb515fa5f
mustaphaouassit <mustaphaoussit60gmail.com>
2022-06-06 21:14:45 +0100

    add part js

---
+++ js/index.js
@@ -0,0 +1,27 @@
+const toggle = document.getElementById("toggle")
+
+const basic = document.getElementById("basic");
+const professional = document.getElementById("professional");
+const master = document.getElementById("master");
+
+let type = "month";
+
+toggle.addEventListener("click",function(){
+
+    if(type == "month")
+    {
+        type = "year";
+        toggle.style.justifyContent = "start"
+        basic.innerHTML = "199.99";
+        professional.innerHTML = "249.99";
+        master.innerHTML = "399.99";
+    }
+    else{
+        type = "month";
+        toggle.style.justifyContent = "end";
+        basic.innerHTML = "19.99";
+        professional.innerHTML = "24.99";
+        master.innerHTML = "39.99";
+    }
+    
+})
